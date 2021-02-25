d94061bd76a5ec72c7b42f79448e58a44e99fd82
liuqitoday <liuqitoday@163.com>
2021-02-25 09:52:01 +0800

    test huoshan

---
+++ task/huoshanCOOKIE.js
@@ -0,0 +1,23 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let hsurlVal= ``
+
+let hsheaderVal= ``
+
+let hsbodyVal= ``
+
+let playurlVal= ``
+
+let playheaderVal= ``
+
+let playbodyVal= ``
+
+let huoshancookie = {
+  hsurlVal: hsurlVal,
+  hsheaderVal: hsheaderVal,
+  hsbodyVal: hsbodyVal,
+  playurlVal: playurlVal,
+  playheaderVal: playheaderVal,
+  playbodyVal: playbodyVal,  
+}
+
+module.exports =  huoshancookie
